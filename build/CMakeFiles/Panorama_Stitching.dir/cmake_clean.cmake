ea95b3180a58eef6ce3e73edd858a4c0255f4c70
Venn@ <you@example.com>
2020-09-16 13:40:10 +0530

    Added all files to the repo.

---
+++ build/CMakeFiles/Panorama_Stitching.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Panorama_Stitching.dir/src/Panorama_Stitching.cpp.o"
+  "Panorama_Stitching.pdb"
+  "Panorama_Stitching"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Panorama_Stitching.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
